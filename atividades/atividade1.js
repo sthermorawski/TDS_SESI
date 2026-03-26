4bb3558e7878a5976d06ce34d198f704171ffd98
sthermorawski <s7herr@gmail.com>
2026-03-26 11:32:47 -0300

    atividade1

---
+++ atividades/atividade1.js
@@ -0,0 +1,7 @@
+
+ 
+   var nome = "raquete";
+   var estoque = 16;
+   var preco = 1700;
+
+console.log("o produto "+ nome +"tem "+ estoque +" unidades em estoque e ele custa "+ preco + " reais.");
